02ccf6a0b4f4060ec30dd4461a2cf253c5aed801
Razrmax <sharpmaxim@gmail.com>
2020-03-15 23:08:04 -0700

    Initial commit

---
+++ ResumeBase/Model/Resume.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeBase.Model
+{
+    class Resume : IComparable<Resume>
+    {
+        private string Uuid { get; set; }
+    
+
+        public Resume(String uuid)
+        {
+            this.Uuid = uuid;
+        }
+
+        
+        public override string ToString()
+        {
+            return Uuid;
+        }
+
+        public int CompareTo(Resume r)
+        {
+            return Uuid.Compare(r.Uuid);
+        }
+
+        public override int GetHashCode()
+        {
+            return Uuid.has;
+        }
+
+        public override boolean Equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass())
+            {
+                return false;
+            }
+            Resume resume = (Resume)o;
+            return Uuid.equals(resume.Uuid);
+        }
+        
+    }
+}
